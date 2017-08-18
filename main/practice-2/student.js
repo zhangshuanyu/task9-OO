6504c0bdcfe946b3d7cc4eb90679d0b61d6880d8
lyn <shandonglyn@163.com>
2017-08-18 22:44:22 +0800

    'finish'

--- main/practice-2/student.js
+++ main/practice-2/student.js
@@ -1,3 +1,16 @@
 const Person = require('./person');
 
-module.exports = class Student {}
+module.exports = class Student extends Person{
+    constructor(name,age,clazz){
+        super(name,age);
+        this.clazz = clazz;
+    }
+
+    introduce(){
+        if(this.clazz.leader === this.id){
+            return super.introduce() + ` I am a Student. I am Leader of Class ${this.clazz.number}.`;
+        }else{
+            return super.introduce() + ` I am a Student. I am at Class ${this.clazz.number}.`;
+        }
+    }
+}
--- main/practice-2/teacher.js
+++ main/practice-2/teacher.js
@@ -1,3 +1,24 @@
 const Person = require('./person');
 
-module.exports = class Teacher {}
+module.exports = class Teacher extends Person{
+    constructor(name,age,clazzes){
+        super(name,age);
+        this.clazzes = clazzes;
+    }
+
+    introduce(){
+        if(this.clazzes.length === 0) {
+            return super.introduce() + ` I am a Teacher. I teach No Class.`;
+        }else {
+            return super.introduce() + ` I am a Teacher. I teach Class ${this.getclazzes()}.`;
+        }
+    }
+
+    getclazzes() {
+        return this.clazzes.reduce((arr, ele) => {
+            arr.push(ele.number);
+            return arr;
+        }, []).join(",");
+    }
+
+}
